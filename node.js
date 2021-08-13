91a305bb115dde5805c2b942cc508b022cab7c09
Aditya-Sinha-7981 <aditya20005sinha@gmail.com>
2021-08-13 21:04:53 +0530

    Added context menu

--- node.js
+++ node.js
@@ -5,7 +5,7 @@ const accordian1 = document.querySelector('.accordianG')
 const accordian2 = document.querySelector('.accordianC')
 const accordian3 = document.querySelector('.accordianT')
 const content = document.querySelector('.accordian-content')
-
+const navbar = document.querySelector('.nav');
 
 
 
@@ -74,3 +74,30 @@ accordianBtn3.addEventListener('click', () => {
         addClass3();
     }
 });
+
+
+
+// Testing
+
+const context = document.querySelector('.contextMenu-outer');
+
+function showContext(show = true){
+    if(show === true){
+        context.style.display = 'block';
+    }else{
+        context.style.display = 'none';
+    }
+}
+
+window.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+
+    showContext()
+
+    context.style.top = e.y + context.offsetHeight > window.innerHeight ? (window.innerHeight - context.offsetHeight) + "px" : (e.y) + "px";
+    context.style.left = e.x + context.offsetWidth > window.innerWidth ? (window.innerWidth - context.offsetWidth) + "px" : e.x + "px";  
+});
+
+window.addEventListener('click', () => {
+    showContext(false);
+})
